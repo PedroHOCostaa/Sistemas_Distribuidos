f7f1aed8878dc0122035b92da26c8a719afc60e1
PedroHOCostaa <pcosta@alunos.utfpr.edu.br>
2024-04-17 16:16:26 -0300

    commit final

--- laboratório-2/ex2/client.py
+++ laboratório-2/ex2/client.py
@@ -1,31 +1,72 @@
+# Aplicação do cliente
+#
+# Autores:
+#          Marcos Rampaso - 2149435
+#          Pedro Costa - 2135663
+
 import socket
 import struct
 import os
 
 HOST = '127.0.0.1'  # Server IP address
-PORT = 65435        # Server port
+PORT = 65435       # Server port
 
 # Função para adicionar um arquivo no servidor
 def add_file(s, filename):
-    if not os.path.exists('client_files'):
+    if not os.path.exists('client_files'):  #Verifica se existe o diretorio.
         os.makedirs('client_files')
-    full_path = os.path.join('client_files', filename)
-    with open(full_path, 'rb') as f:
-        data = f.read() # Le o arquivo para enviar ao servidor
-    base_filename = os.path.basename(filename)  # Extrai o nome do arquivo do caminho completo
-    s.sendall(struct.pack('>BBB', 1, 1, len(base_filename)) + base_filename.encode() + struct.pack('>I', len(data)) + data) # evia o comando ADDFILE para o servidor, conforme o cabeçalho definido
+    try:
+        full_path = os.path.join('client_files', filename)
+        with open(full_path, 'rb') as f:
+            data = f.read() # Le o arquivo para enviar ao servidor
+        base_filename = os.path.basename(filename)  # Extrai o nome do arquivo do caminho completo
+        s.sendall(struct.pack('>BBB', 1, 1, len(base_filename)) + base_filename.encode() + struct.pack('>I', len(data)) + data) # evia o comando ADDFILE para o servidor, conforme o cabeçalho definido
+        response = struct.unpack('>BBB', s.recv(3)) # Recebe a resposta do servidor
+        print(f"Response: {response[0]} {response[1]} {response[2]}")
+        if response[2] == 2 :
+                print(f"Erro na função ADDFILE")
+    except:                     #Caso o usuário escolha um arquivo que não existe 
+        print("Arquivo inexistente!")
+
 
 # Funçao para deletar um arquivo no servidor
 def delete_file(s, filename):
     s.sendall(struct.pack('>BBB', 1, 2, len(filename)) + filename.encode()) # Envia o comando DELETE para o servidor, conforme o cabeçalho definido
+    response = struct.unpack('>BBB', s.recv(3)) # Recebe a resposta do servidor
+    print(f"Response: {response[0]} {response[1]} {response[2]}")
+    if response[2] == 2 :
+        print(f"Erro na função DELETE")
+
 
 # FUuçao para receber a lista de arquivos do servidor
 def get_files_list(s):
     s.sendall(struct.pack('>BBB', 1, 3, 0)) # Envia o comando GETFILESLIST para o servidor, conforme o cabeçalho definido
+    
+    
+    response = struct.unpack('>BBB', s.recv(3)) # Recebe a resposta do servidor
+    print(f"Response: {response[0]} {response[1]} {response[2]}")
+    if response[2] == 2 :
+        print(f'Erro na execução da função GETFILESLIST')
+        return
+    qtd_de_arquivos = struct.unpack('>H', s.recv(2))[0]
+    print(f"Quantidade de Arquivos {qtd_de_arquivos}")
+    
+    
+    for i in range(qtd_de_arquivos):
+        tamanho_nome = struct.unpack('>B', s.recv(1))[0]
+        nome = s.recv(tamanho_nome).decode()
+        print(f"\ntamanho do nome {tamanho_nome} nome {nome}")
+
 
 # Funçao para receber um arquivo do servidor
 def get_file(s, filename):
     s.sendall(struct.pack('>BBB', 1, 4, len(filename)) + filename.encode()) # Envia o comando GETFILE para o servidor, conforme o cabeçalho definido
+
+    response = struct.unpack('>BBB', s.recv(3)) # Recebe a resposta do servidor
+    print(f"Response: {response[0]} {response[1]} {response[2]}")
+    if response[2] == 2 :
+        print(f"Erro na função GETFILE")
+        return
 
     file_size = struct.unpack('>I', s.recv(4))[0]   # Recebe o tamanho do arquivo onde o servidor envia o tamanho do arquivo, que é um inteiro de 4 bytes
                                                     
@@ -38,27 +79,29 @@ def get_file(s, filename):
             data = s.recv(min(4096, file_size))        # Recebe os dados do arquivo em pedaços de 4096 bytes
             f.write(data)                              # Escreve os dados no arquivo
             file_size -= len(data)                     # Decrementa o tamanho do arquivo para controlar o loop
+    f.close
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # Cria um socket TCP
-    s.connect((HOST, PORT))                                     # Conecta ao servidor
-    while True:
-        command = input("Comandos > (ADDFILE, DELETE, GETFILESLIST, GETFILE) ou 'STOP' para sair: ")
-        if command == 'STOP':                            # Se o comando for STOP, encerra a conexão
-            print("Encerrando conexao...")
-            break
-        elif command == 'ADDFILE':
-            filename = input("Insira o nome do arquivo: ")
-            add_file(s, filename)                      # Chama a função add_file para enviar o arquivo para o servidor
-        elif command == 'DELETE':
-            filename = input("Insira o nome do arquivo: ")
-            delete_file(s, filename)               # Chama a função delete_file para deletar o arquivo no servidor
-        elif command == 'GETFILESLIST':
-            get_files_list(s)           # Chama a função get_files_list para receber a lista de arquivos do servidor
-        elif command == 'GETFILE':
-            filename = input("Insira o nome do arquivo: ")
-            get_file(s, filename)       # Chama a função get_file para receber um arquivo do servidor
-        else:
-            print("Comando invalido, os comandos disponiveis sao:")
-            continue
-        data = s.recv(1024) # Recebe a resposta do servidor
-        print(f"Recebido: {data.decode()}")
+    try:
+        s.connect((HOST, PORT))                                     # Conecta ao servidor
+        while True:
+            command = input("Comandos > (ADDFILE, DELETE, GETFILESLIST, GETFILE) ou 'STOP' para sair: ")
+            if command == 'STOP':                            # Se o comando for STOP, encerra a conexão
+                print("Encerrando conexao...")
+                break
+            elif command == 'ADDFILE':
+                filename = input("Insira o nome do arquivo: ")
+                add_file(s, filename)                      # Chama a função add_file para enviar o arquivo para o servidor
+            elif command == 'DELETE':
+                filename = input("Insira o nome do arquivo: ")
+                delete_file(s, filename)               # Chama a função delete_file para deletar o arquivo no servidor
+            elif command == 'GETFILESLIST':
+                get_files_list(s)           # Chama a função get_files_list para receber a lista de arquivos do servidor
+            elif command == 'GETFILE':
+                filename = input("Insira o nome do arquivo: ")
+                get_file(s, filename)       # Chama a função get_file para receber um arquivo do servidor
+            else:
+                print("Comando invalido, os comandos disponiveis sao:")
+                continue
+    except:
+        print("Erro ao se conectar ao servidor")
--- laboratório-2/ex2/server.py
+++ laboratório-2/ex2/server.py
@@ -1,69 +1,120 @@
+# Aplicação do servidor
+#
+# Autores:
+#          Marcos Rampaso - 2149435
+#          Pedro Costa - 2135663
 import socket
 import struct
 import os
 import logging
+import threading
+
 
 HOST = '127.0.0.1'  # Server IP address
-PORT = 65435        # Server port
+PORT = 65435       # Server port
+
+#Todas as funções dentro do servidor estão com try e except, para que se houver algum erro
+#seja enviado ao cliente um cabeçalho indicando que houve um erro.
+
+# Configurando o logging para enviar mensagens para um arquivo chamado 'logfile.log'
+
+
+logging.basicConfig(filename='./server_files/server.log', level=logging.INFO)
 
 def add_file(conn, filename, file_size):
-    if not os.path.exists('server_files'):
-        os.makedirs('server_files')
-    filename = os.path.join('server_files', filename)
-    with open(filename, 'wb') as f:
-        while file_size:
-            chunk = conn.recv(min(file_size, 1024))
-            f.write(chunk)
-            file_size -= len(chunk)
-    logging.info(f'File {filename} added')
-    conn.sendall(struct.pack('>BBB', 2, 1, 1))  # SUCCESS
+    try:
+        if not os.path.exists('server_files'):
+            os.makedirs('server_files')
+        filename = os.path.join('server_files', filename)
+        with open(filename, 'wb') as f:
+            while file_size:
+                chunk = conn.recv(min(file_size, 1024))
+                f.write(chunk)
+                file_size -= len(chunk)
+        f.close()
+        logging.info(f'File {filename} added')
+        conn.sendall(struct.pack('>BBB', 2, 1, 1))  # SUCCESS
+    except:
+        conn.sendall(struct.pack('>BBB', 2, 1, 1))  # ERRO
+        logging.info(f'Erro no ADDFILE')
 
+#Função para deletar um arquivo do servidor
 def delete_file(conn, filename):
-    filename = os.path.join('server_files', filename)
-    os.remove(filename)
-    logging.info(f'File {filename} deleted')
-    conn.sendall(struct.pack('>BBB', 2, 2, 1))  # SUCCESS
+    try:
+        filename = os.path.join('server_files', filename)
+        os.remove(filename)
+        logging.info(f'File {filename} deleted')
+        conn.sendall(struct.pack('>BBB', 2, 2, 1))  # SUCCESS
+    except:
+        conn.sendall(struct.pack('>BBB', 2, 2, 2))  # Error
+        logging.info(f'Error no DELETEFILE')
 
-def get_files_list(conn):
-    files = os.listdir('server_files')
-    for file in files:
-        conn.sendall(file.encode() + b'\n')
-    logging.info('File list sent')
-    conn.sendall(struct.pack('>BBB', 2, 3, 1))  # SUCCESS
 
+#Função para recuperar os arquivos do servidor
+def get_files_list(conn):    
+    try:
+        files = os.listdir('server_files')
+        count = len(files)
+        resultado_parcial = count.to_bytes(2,'big')
+        for file in files:
+            resultado_parcial = resultado_parcial + len(file).to_bytes(1, 'big') + file.encode()
+        result = struct.pack('>BBB', 2, 3, 1) + resultado_parcial
+        conn.sendall(result)
+        logging.info('File list sent ')
+    except:
+        conn.sendall(struct.pack('>BBB', 2, 3, 2))
+
+#Função para envoar o arquivo que será baixado
 def get_file(conn, filename):
     filename = os.path.join('server_files', filename)
-    with open(filename, 'rb') as f:
-        data = f.read()
-    conn.sendall(struct.pack('>I', len(data)))
-    conn.sendall(data)
-    logging.info(f'File {filename} sent')
-    conn.sendall(struct.pack('>BBB', 2, 4, 1))  # SUCCESS
+    try:
+        with open(filename, 'rb') as f:
+            data = f.read()
+        f.close()
+        conn.sendall(struct.pack('>BBB', 2, 4, 1))  # SUCCESS
+        conn.sendall(struct.pack('>I', len(data)))
+        conn.sendall(data)
+        logging.info(f'File {filename} sent')
+    except:
+        conn.sendall(struct.pack('>BBB', 2, 4, 2))  # ERROR
+
+def handle_client(conn, addr):
+    logging.info(f'Connected by {addr}')
+    while True:
+        try:
+            msg_type, cmd_id, filename_size = struct.unpack('>BBB', conn.recv(3))
+            filename = conn.recv(filename_size).decode()
+            if cmd_id == 1:  # ADDFILE
+                file_size, = struct.unpack('>I', conn.recv(4))
+                print(f"ADDFILE addr = {addr}")
+                add_file(conn, filename, file_size)
+            elif cmd_id == 2:  # DELETE
+                print(f"DELETE addr = {addr}")
+                delete_file(conn, filename)
+            elif cmd_id == 3:  # GETFILESLIST
+                print(f"GETFILESLIST addr = {addr}")
+                get_files_list(conn)
+            elif cmd_id == 4:  # GETFILE
+                print(f"GETFILE addr = {addr}")
+                get_file(conn, filename)
+        except struct.error:
+            break
 
 def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen()
-
-    while True:
-        conn, addr = server.accept()
-        with conn:
-            logging.info(f'Connected by {addr}')
-            while True:
-                try:
-                    msg_type, cmd_id, filename_size = struct.unpack('>BBB', conn.recv(3))
-                    filename = conn.recv(filename_size).decode()
-                    if cmd_id == 1:  # ADDFILE
-                        file_size, = struct.unpack('>I', conn.recv(4))
-                        add_file(conn, filename, file_size)
-                    elif cmd_id == 2:  # DELETE
-                        delete_file(conn, filename)
-                    elif cmd_id == 3:  # GETFILESLIST
-                        get_files_list(conn)
-                    elif cmd_id == 4:  # GETFILE
-                        get_file(conn, filename)
-                except struct.error:
-                    break
-
-if __name__ == '__main__':
-    start_server()
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:                        #testa a porta padrão, caso falhe, testa secundária, caso falhe de novo informa ao log que houve erro.
+            server.bind((HOST, PORT))
+            server.listen()
+        except:
+            server.bind(HOST, 65433)
+            server.listen
+        while True:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            print(f"conexoes ativas: {threading.active_count() - 1}")
+    except:
+        print("Erro ao inicializar o servidor")
+        logging.info(f'ERROR starting the server')
+start_server()
